9c792bd7c9a1b3971ab73ee677454bd3a2496e8d
stan10aviles <stan10aviles@hotmail.com>
2025-11-10 17:49:29 -0500

    Created feedback entity/model

---
+++ src/main/java/com/joey/stanley/group/project/feedback_api/entity/Feedback.java
@@ -0,0 +1,33 @@
+package com.joey.stanley.group.project.feedback_api.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Check;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name="feedback")
+@Check(constraints = "rating BETWEEN 1 AND 5")
+public class Feedback {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, length = 36)
+    private String memberId;
+
+    @Column(nullable = false, length = 80)
+    private String providerName;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @Column(nullable = false, length = 200)
+    private String comment;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private OffsetDateTime submittedAt;
+}
