37b373f3928e5b518fcdd3755e0faefe675f0198
stan10aviles <stan10aviles@hotmail.com>
2025-11-10 12:41:12 -0500

    Initialized feedback-api backend project

---
+++ src/test/java/com/joey/stanley/group/project/feedback_api/FeedbackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joey.stanley.group.project.feedback_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
