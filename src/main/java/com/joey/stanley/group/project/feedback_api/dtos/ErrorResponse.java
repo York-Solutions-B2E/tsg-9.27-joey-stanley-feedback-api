44bcdad2871d67391b85397f50473ebacc5bcdba
Joseph Cramsey <josephcsoftware@gmail.com>
2025-11-11 15:20:02 -0500

    Added DTOs

---
+++ src/main/java/com/joey/stanley/group/project/feedback_api/dtos/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.joey.stanley.group.project.feedback_api.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ErrorResponse {
+
+    private String message;
+}
